c68a1b26ec126fa273f6def9832fa1ae328b67d7
Richard Altamirano <61747656+Richard577V@users.noreply.github.com>
2022-01-18 01:14:26 -0500

    IGUPROYECTOINICIO

---
+++ IGU/ModeloBD/Repositorio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloBD
+{
+    public class Repositorio: DbContext 
+    {
+        // Configuracion de las entidades
+        public DbSet<Estudiante> Estudiantes { get; set; }
+        public DbSet<NotaGrado> NotaGrados { get; set; }
+        public DbSet<NotaPrueba> NotaPruebas { get; set; }
+        public DbSet<Prueba> Pruebas { get; set; }
+    }
+}
