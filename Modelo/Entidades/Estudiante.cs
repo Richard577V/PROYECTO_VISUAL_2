260fb16ae0ae130134c0d3083960374b3c3f6a03
Richard Altamirano <61747656+Richard577V@users.noreply.github.com>
2022-01-18 00:46:49 -0500

    INICIODELPROYECTO

---
+++ Modelo/Entidades/Estudiante.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.Entidades
+{
+    public class Estudiante
+    {
+        public int Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public float Edad { get; set; }
+        public string Correo { get; set; }
+
+
+    }
+}
